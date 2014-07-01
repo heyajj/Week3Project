c178bb8dd66f5dd7a911a1037e1374b78756e677
heyajj <ajanich@ajanich-macbookpro.roam.corp.google.com>
2014-07-01 10:11:47 -0700

    Initial Commit

---
+++ Week3Project/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Week3Project
+//
+//  Created by Andrew Janich on 7/1/14.
+//  Copyright (c) 2014 Google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Week3ProjectTests/Week3ProjectTests.m
@@ -0,0 +1,34 @@
+//
+//  Week3ProjectTests.m
+//  Week3ProjectTests
+//
+//  Created by Andrew Janich on 7/1/14.
+//  Copyright (c) 2014 Google. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Week3ProjectTests : XCTestCase
+
+@end
+
+@implementation Week3ProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
