6e3cfe79cb916ce0301466ae50f369facf8461d6
heyajj <ajanich@ajanich-macbookpro.roam.corp.google.com>
2014-07-01 11:16:47 -0700

    initial comment

---
+++ MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Week3Project
+//
+//  Created by Andrew Janich on 7/1/14.
+//  Copyright (c) 2014 Google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ MainViewController.m
@@ -0,0 +1,42 @@
+//
+//  MainViewController.m
+//  Week3Project
+//
+//  Created by Andrew Janich on 7/1/14.
+//  Copyright (c) 2014 Google. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+
+//color hex import
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
